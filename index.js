b404dc2a68e61ee0d215c40f4bf236e5e8bdd525
Mariano Corvatta <62441854+marianocorvatta@users.noreply.github.com>
2023-08-17 15:57:55 +0200

    add webhook method

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ app.get("/", (req, res) => {
   res.send("Hello dolar-contact !");
 });
 
-app.post("/webhook", (req, res, next) => {
-  console.log("message body", req.body);
+app.post("/webhooks", (req, res) => {
+  const body = JSON.parse(req.body);
+  if (body.field !== "messages") {
+    // not from the messages webhook so dont process
+    return res.sendStatus(400);
+  }
+  const review = {
+    phonenumber: body.value.metadata.display_phone_number,
+    review: body.value.messages
+      .map((message) => message.text.body)
+      .join("\n\n"),
+  };
 
+  console.log("reviewInfo", review);
   res.sendStatus(200);
 });
 
