a55a47aa0e559f01d82782d1b11e3583b5983208
Mariano Corvatta <62441854+marianocorvatta@users.noreply.github.com>
2023-08-17 15:43:55 +0200

    add post method

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.get("/", (req, res) => {
   res.send("Hello dolar-contact !");
 });
 
+app.post("/webhook", (req, res, next) => {
+  console.log("message body", req.body);
+
+  res.sendStatus(200);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
