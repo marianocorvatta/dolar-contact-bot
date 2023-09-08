30dd7452ab287b7f38056464c36fcec714f5c80c
Mariano Corvatta <62441854+marianocorvatta@users.noreply.github.com>
2023-09-08 22:49:38 +0200

    migrate to telegram api

--- index.js
+++ index.js
@@ -6,64 +6,52 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-const { sendMessage, getTextMessageInput } = require("./sendmessage.js");
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.TELEGRAM_API_KEY;
+
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/dolar [number]"
+bot.onText(/\/dolar (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1];
+  bot.sendMessage(chatId, 'Proccesing...');
+
+  // send back the amount in USD
+  bot.sendMessage(chatId, resp);
+});
+
+
+// Matches "/peso [number]"
+bot.onText(/\/peso (.+)/, (msg, match) => {
+
+  const chatId = msg.chat.id;
+  const resp = match[1];
+  bot.sendMessage(chatId, 'Proccesing...');
+
+  // send back the amount in ARS
+  bot.sendMessage(chatId, resp);
+});
+
+// Matches "/euro [number]"
+bot.onText(/\/euro (.+)/, (msg, match) => {
+
+  const chatId = msg.chat.id;
+  const resp = match[1];
+  bot.sendMessage(chatId, 'Proccesing...');
+
+  // send back the amount in EUR
+  bot.sendMessage(chatId, resp);
+});
+
+// bot.on('message', (msg) => {
+//   const chatId = msg.chat.id;
+//   bot.sendMessage(chatId, 'Received your message');
+// });
 
 app.get("/", (req, res) => {
-  res.send("Hello dolar-contact !");
-});
-
-const token = process.env.TOKEN;
-
-app.get("/test", async (req, res) => {
-  try {
-    const resp = await sendMessage()
-    console.log('resp', resp.data)
-    res.status(200).send("Message sent!");
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-app.get("/webhooks", async (req, res) => {
-  console.log("verify_token", req.query["hub.verify_token"]);
-  if (
-    req.query["hub.mode"] == "subscribe" &&
-    req.query["hub.verify_token"] == token
-  ) {
-    res.send(req.query["hub.challenge"]);
-  } else {
-    res.sendStatus(400);
-  }
-});
-
-app.post("/webhook", async (req, res) => {
-  const body = JSON.parse(req.body);
-  // if (body.field !== "messages") {
-  //   // not from the messages webhook so dont process
-  //   return res.sendStatus(400);
-  // }
-  // const review = {
-  //   phonenumber: body.value.metadata.display_phone_number,
-  //   review: body.value.messages
-  //     .map((message) => message.text.body)
-  //     .join("\n\n"),
-  // };
-
-  // var data = getTextMessageInput(process.env.RECIPIENT_WAID, 'Welcome to the Movie Ticket Demo App for Node.js!');
-  
-  // sendMessage(data)
-  //   .then(function (response) {
-  //     res.sendStatus(200);
-  //     return;
-  //   })
-  //   .catch(function (error) {
-  //     console.log(error);
-  //     console.log(error.response.data);
-  //     res.sendStatus(500);
-  //     return;
-  //   });
-  const resp = await sendMessage()
-  res.sendStatus(200);
+  res.send("Hello dolar-contact-bot !");
 });
 
 app.listen(port, () => {
